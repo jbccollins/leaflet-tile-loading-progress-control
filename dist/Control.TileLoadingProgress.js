29e89037496ec02218359671412762d1f7e9017a
James Collins <jbccollins@gmail.com>
2019-07-24 13:57:43 -0400

    v1.0.7

--- dist/Control.TileLoadingProgress.js
+++ dist/Control.TileLoadingProgress.js
@@ -1,5 +1,5 @@
 /* @preserve
- * Leaflet Control TileLoadingProgress 1.0.6
+ * Leaflet Control TileLoadingProgress 1.0.7
  * https://github.com/jbccollins/leaflet-tile-loading-progress-control
  *
  * Copyright (c) 2018 James Collins
